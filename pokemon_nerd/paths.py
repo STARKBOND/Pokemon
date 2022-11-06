5473ae40945d4bb9b1c1af187e38f57e7da0f3a2
Cyber Scholar <48360032+STARKBOND@users.noreply.github.com>
2022-11-05 20:11:01 -0600

    Depending whether package is cloned from github or installed with pip, there is or isn't a top level directory. This patch addresses that.

--- pokemon_nerd/paths.py
+++ pokemon_nerd/paths.py
@@ -1,10 +1,10 @@
 import os, sys, pokemon_nerd
 [sys.path.append(i) for i in ['.', '..']] # allow imports from root dir
 
-ROOT_DIR =  os.path.abspath(os.path.join(pokemon_nerd.__file__ ,"../../.."))
-if 'pokemon_nerd' not in ROOT_DIR: ROOT_DIR += 'pokemon_nerd' # Windows 
-APP_DIR = os.path.join(ROOT_DIR,'pokemon_nerd')
-DATA_DIR = os.path.join(APP_DIR, 'data')
+ROOT_DIR =  os.path.abspath(os.path.join(pokemon_nerd.__file__ ,"../.."))
+# if 'pokemon_nerd' not in ROOT_DIR: ROOT_DIR += '\\pokemon_nerd' # Windows 
+ROOT_DIR = os.path.join(ROOT_DIR,'pokemon_nerd')
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
 CSV_PATH = os.path.join(DATA_DIR, 'pokemon.csv') 
 DICTIONARY_PATH = os.path.join(DATA_DIR, 'pocketsphinx/1827.dic')
 LANGUAGE_MODEL_PATH = os.path.join(DATA_DIR, 'pocketsphinx/1827.lm')
