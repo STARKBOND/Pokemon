80949dcf85785b491672b58b700ff3bf8922e9e8
Cyber Scholar <48360032+STARKBOND@users.noreply.github.com>
2022-11-06 08:47:30 -0700

    Made notes in logic.py concerning implementation.

--- pokemon_nerd/logic.py
+++ pokemon_nerd/logic.py
@@ -14,3 +14,16 @@ def spawn_pokemon(name: str):
         pokemon = globals()[type[0]]() + globals()[type[1]]()
     
     return pokemon
+
+# listen() could be an event that Question is listening to
+# Question could have 2 types
+    # What is <stat> <type/pokemon>?
+    # What (does/is) <type/pokemon> <stat>?
+# Each with a corresponding event handler
+    # if SUBSTRING(first 3 words of question) in "what is" or "what's" in 
+        # if SUBSTRING(the rest of the question) in <type>
+            # create type
+            # or if SUBSTRING(the rest of the question) in <type> <type>
+                # create dualType
+        # if SUBSTRING(the rest of the question) in <pokemon>
+            # spawn pokemon
